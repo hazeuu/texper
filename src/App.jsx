5e67519dc891c2371cf85b39cc1ada54732f8f70
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2025-12-23 10:51:17 +0700

    Fix routes and update ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -12,24 +12,29 @@ import Reminder from "./pages/internal/jsx/reminder";
 import Account from "./pages/internal/jsx/account";
 import ProtectedRoute from "./ProtectedRoute";
 import QandA from "./pages/QandA";
-import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Layout_home><Home /></Layout_home>} />
         <Route path="/About" element={<Layout><About /></Layout>} />
-        <Route path="/Login" element={<Layout_home><Login/></Layout_home>}/>
-        <Route path="/Signup" element={<Layout><Signup/></Layout>}/>
-        <Route path="/Forgot_passwords" element={<Layout><Forgot_passwords/></Layout>}/>
-      </Routes>
-      <Routes element={<ProtectedRoute/>}>
-        <Route path="/Dashboard" element={<Layout_home><Dashboard/></Layout_home>}/>
-        <Route path="/calendar" element={<Layout_home><Calendar/></Layout_home>}/>
-        <Route path="/reminder" element={<Layout_home><Reminder/></Layout_home>}/>
-        <Route path="/account" element={<Layout_home><Account/></Layout_home>}/>
-        <Route path="/QandA" element={<Layout_home><QandA/></Layout_home>}/>
+        <Route path="/Login" element={<Layout_home><Login /></Layout_home>} />
+        <Route path="/Signup" element={<Layout><Signup /></Layout>} />
+        <Route path="/Forgot_passwords" element={<Layout><Forgot_passwords /></Layout>} />
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/Dashboard" element={<Layout_home><Dashboard /></Layout_home>} />
+          <Route path="/calendar" element={<Layout_home><Calendar /></Layout_home>} />
+          <Route path="/reminder" element={<Layout_home><Reminder /></Layout_home>} />
+          <Route path="/account" element={<Layout_home><Account /></Layout_home>} />
+          <Route path="/QandA" element={<Layout_home><QandA /></Layout_home>} />
+        </Route>
+
+        {/* Fallback 404 */}
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
   );
