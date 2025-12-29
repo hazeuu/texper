e9106a8548e2d827d0ac35d90a7888cfd0321382
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2025-12-29 18:53:02 +0700

    Fix Router chồng Router, cập nhật App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './pages/internal/jsx/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <HashRouter>
+  
     <BrowserRouter basename="/texper">
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  
+  </HashRouter>
 )
