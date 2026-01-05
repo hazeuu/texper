c754a5a6b49a5a221ca41d0f2b1a6753f61fabe3
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2026-01-06 03:34:26 +0700

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/texper/', // TÊN REPO GITHUB
+  base: '/texper/', // TÊN REPO GITHUB
 })
