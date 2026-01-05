45efbd867de684218befe22b1affec2adf3a3fca
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2026-01-06 03:01:30 +0700

    edit proxy name

--- script/start.sh
+++ script/start.sh
@@ -1,11 +1,16 @@
 echo "=== CHECK PROXY FILE ==="
-ls -l ../../cloud_sql_proxy
-file ../../cloud_sql_proxy
+ls -l ../../cloud_sql_proxy.linux.amd64
+file ../../cloud_sql_proxy.linux.amd64
 echo "=== END CHECK ==="
 
 echo "$GCP_SERVICE_ACCOUNT_JSON" > /tmp/key.json
-../../cloud_sql_proxy -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
+
+chmod +x ../../cloud_sql_proxy.linux.amd64
+
+../../cloud_sql_proxy.linux.amd64 -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
 echo "Proxy started in background..."
+
 sleep 5
+
 echo "Starting Node app..."
 node server.js
