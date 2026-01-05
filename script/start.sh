56c7ad1cb2d337be60676775cfa002d197eecb40
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2026-01-06 02:56:52 +0700

    update start sh and rename prxy

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,11 @@
+echo "=== CHECK PROXY FILE ==="
+ls -l ../../cloud_sql_proxy
+file ../../cloud_sql_proxy
+echo "=== END CHECK ==="
 
 echo "$GCP_SERVICE_ACCOUNT_JSON" > /tmp/key.json
-
-./cloud_sql_proxy -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
-
-sleep 2
-
+../../cloud_sql_proxy -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
+echo "Proxy started in background..."
+sleep 5
+echo "Starting Node app..."
 node server.js
