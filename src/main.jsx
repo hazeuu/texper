94767d547c4318620dba44f87c5ee59a9781b9a8
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2026-01-06 10:30:37 +0700

    add font Lato

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
 
 import App from './App.jsx'
 import './index.css'
