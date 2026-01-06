07c908004e6dc969d7739a484d28077de1fdd689
hazeuu <Anh.NT223752@sis.hust.edu.vn>
2026-01-06 09:42:20 +0700

    edit name proxy in start

--- script/start.sh
+++ script/start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo "=== CHECK PROXY FILE ==="
-ls -l ../cloud_sql_proxy.linux.amd64
-file ../cloud_sql_proxy.linux.amd64
+ls -l ./cloud-sql-proxy.linux.amd64
+file ./cloud-sql-proxy.linux.amd64
 echo "=== END CHECK ==="
 
 echo "$GCP_SERVICE_ACCOUNT_JSON" > /tmp/key.json
 
-chmod +x ../cloud_sql_proxy.linux.amd64
+chmod +x ./cloud-sql-proxy.linux.amd64
 
-../cloud_sql_proxy.linux.amd64 -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
+./cloud-sql-proxy.linux.amd64 -instances=$PROJECT_ID:$REGION:$INSTANCE_NAME=tcp:3306 -credential_file=/tmp/key.json &
 echo "Proxy started in background..."
 
 sleep 5
